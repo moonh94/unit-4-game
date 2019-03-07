3d2b89b94e36b8b16d78c13ce0a1fe02a9620f4a
Han Moon <hanmoon@Hans-MacBook-Pro.local>
2019-03-06 21:06:34 -0700

    random number reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,30 +2,48 @@
 var wins =0;
 var lose = 0;
 
+
 //creating a target number
-var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+var targetNumber = Math.floor(Math.random() * (120 - 19 + 1)) + 19; 
     $("#number-to-guess").text(targetNumber);
+
+
+var reset = function() {
+    $("#number-to-guess").empty();
+    
+   
+}
 
 //giving crystals a random number
 var numberOptions = [Math.floor(Math.random() * (12)), Math.floor(Math.random() * (12)), Math.floor(Math.random() * (12)), Math.floor(Math.random() * (12))];
 var increment = numberOptions[Math.round(Math.random())];
 
-// var randomValue1 = Math.floor(Math.random() * (11)) + 1;
-// var randomValue2 = Math.floor(Math.random() * (11)) + 1;
-// var randomValue3 = Math.floor(Math.random() * (11)) + 1;
-// var randomValue4 = Math.floor(Math.random() * (12)) + 1;
-
 
 for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWR7WP7etKttEFNOu1bMApQJxZd9CyzzBnPR9emUDeYg2I0Kav");
+    imageCrystal.attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqjkn0OaLDc9w9qlVH5FRNgevIXPC71hJFIAJ-OOY5b_hDtn3u");
     imageCrystal.attr("data-crystalValue", numberOptions[i]);
     $("#crystals").append(imageCrystal);
-}
+
+    //used the following code to insert 4 diff pics, with no luck
+    // var images = [
+    //    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqjkn0OaLDc9w9qlVH5FRNgevIXPC71hJFIAJ-OOY5b_hDtn3u",
+    //    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOSiIVbVtbgh7sYS3UYqF2dvEmWOiDw0K77TB7-KccK9c0qBhc.jpg",
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrcKUnWC-7mDzJmH6kkoqatK5CfvvE04qVd33IoJZ3aJBLeBpgA",
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWR7WP7etKttEFNOu1bMApQJxZd9CyzzBnPR9emUDeYg2I0Kav",
+    // ]
+    // var imageCrystal = $("<img>");
+    // imageCrystal.addClass("crystal-image");
+    // imageCrystal.attr("data-crystalValue", numberOptions[i]);
+    // imageCrystal.html(images);
+
+$("#crystals").append(imageCrystal);
+};
 
 
-//this counter tracks user's total
+
+//this counter tracks users total
 var counter = 0;
 
 //click events for crystals
@@ -39,12 +57,20 @@ $(".crystal-image").on("click", function() {
         wins++;
         $("#wins").text(wins);
         counter=0;
+        reset(); 
+        targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#number-to-guess").text(targetNumber);
     
     } else if (counter > targetNumber) {
         lose++;
         $("#losses").text(lose);
         counter=0;
-        
+        reset();
+        targetNumber=Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+        $("#number-to-guess").text(targetNumber);
+
+       
+         
         
     };
    
